82a8b9def3645b51e65593d1f0fe396deb48e0c4
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-03-25 23:43:39 -0400

    separate config, trying to set indexed db

---
+++ config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: 'AIzaSyCzOAIvnEkjCinXCLC6yDmb0YjAeOGI7_o',
+  authDomain: 'auth-example-373b4.firebaseapp.com',
+  databaseURL: 'https://auth-example-373b4.firebaseio.com',
+  projectId: 'auth-example-373b4',
+  storageBucket: 'auth-example-373b4.appspot.com',
+  messagingSenderId: '583998876852'
+}
--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,8 +2,11 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
+import { openDB } from 'idb'
+import { config } from '../../config'
+
 describe('Firebase email + password authentication', () => {
-  const username = 'jane@xyz123.com'
+  const email = 'jane@xyz123.com'
   const password = 'password123'
 
   context('via UI', () => {
@@ -27,7 +30,7 @@ describe('Firebase email + password authentication', () => {
       cy.contains('#quickstart-sign-in-status', 'Unknown').should('be.visible')
       cy.contains('#quickstart-sign-in', 'Sign In')
 
-      cy.get('#email').type(username)
+      cy.get('#email').type(email)
       cy.get('#password').type(password)
 
       cy.get('#quickstart-sign-in').click()
@@ -36,6 +39,78 @@ describe('Firebase email + password authentication', () => {
         'be.visible'
       )
       cy.contains('#quickstart-sign-in', 'Sign out')
+    })
+  })
+
+  context.only('via cy.request', () => {
+    beforeEach(() => indexedDB.deleteDatabase('firebaseLocalStorageDb'))
+
+    beforeEach(() => {
+      // from Firebase config, same as index.html
+      const endpoint = `https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=${
+        config.apiKey
+      }`
+      cy.request({
+        method: 'POST',
+        url: endpoint,
+        form: true,
+        body: {
+          email,
+          password
+        }
+      })
+        .its('body')
+        // sure, use async / await functions for 3rd party code
+        // parcel bundler transpiles them nicely to promise-returning code
+        .then(async body => {
+          const db = await openDB('firebaseLocalStorageDb', 1, {
+            upgrade (db) {
+              db.createObjectStore('firebaseLocalStorage', {
+                keyPath: 'fbase_key',
+                autoIncrement: false
+              })
+            }
+          })
+
+          const fbase_key = `firebase:authUser:${config.apiKey}:[DEFAULT]`
+          const value = {
+            appName: '[DEFAULT]',
+            // createdAt: String(+new Date()),
+            displayName: body.displayName,
+            email: body.email,
+            emailVerified: false,
+            isAnonymous: false,
+            // lastLoginAt: String(+new Date()),
+
+            providerData: [
+              {
+                displayName: body.displayName,
+                email: body.email,
+                providerId: 'password',
+                uid: body.email
+              }
+            ],
+            apiKey: config.apiKey,
+            authDomain: config.authDomain,
+
+            stsTokenManager: {
+              accessToken: body.idToken,
+              apiKey: config.apiKey
+              // expirationTime: +new Date() + 1000000
+            },
+            uid: body.localId
+          }
+          console.log(value)
+
+          await db.add('firebaseLocalStorage', {
+            fbase_key,
+            value
+          })
+        })
+    })
+
+    it('works', () => {
+      cy.visit('/')
     })
   })
 })
